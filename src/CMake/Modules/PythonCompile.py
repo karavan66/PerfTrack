70a1534258908bedc46104a4fb69f0e3843a034d
Heath Harrelson <heath.harrelson@gmail.com>
2013-08-08 13:41:52 -0700

    Use Better Python Detection
    Adds a set of CMake files from https://github.com/lyricat/Hotot and
    modifies the project to use them, adding the ability to find the
    system's Python site-packages directory, compile python programs, etc.
    
    Changes:
    
     * src/CMakeLists.txt: Move module inclusion, python detecion to top-
       level CMake config.
     * src/GUI/CMakeLists.txt: Use python detection from top-level config.
     * src/CMake/Modules/FindLibPython.py: New file from GitHub.
     * src/CMake/Modules/FindPythonLibrary.cmake: New file from GitHub.
     * src/CMake/Modules/PythonCompile.py: New file from GitHub.
     * src/CMake/Modules/PythonMacros.cmake: New file from GitHub.

---
+++ src/CMake/Modules/PythonCompile.py
@@ -0,0 +1,6 @@
+# Downloaded from: https://github.com/lyricat/Hotot
+
+# By Simon Edwards <simon@simonzone.com>
+# This file is in the public domain.
+import py_compile, sys
+sys.exit(py_compile.main())
